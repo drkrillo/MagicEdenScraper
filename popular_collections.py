414c888f6b40670334ad54bb920c30524020272d
Juan Pasutti <juanpasutti@clear-tech.com>
2022-09-27 00:23:02 -0300

    modified script to scrape more popular cols

--- popular_collections.py
+++ popular_collections.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 import time 
 import pandas as pd
+import sys
 
 pd.options.display.max_columns = None
+pd.options.display.max_rows = None
 
 # Definimos las opciones de Chrome
 chrome_options = webdriver.ChromeOptions()
@@ -19,23 +21,31 @@ wd = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
 wd.get('https://magiceden.io/popular-collections')
 
 wd.maximize_window()
-time.sleep(5)
+time.sleep(2)
 
-#scrollable_element = wd.find_element('xpath','//div[@class="dark-scroll-bar"]')
-#wd.execute_script("arguments[0].scroll(0, arguments[0].scrollHeight);", scrollable_element)
-#time.sleep(5)
+popular_collections = pd.DataFrame(columns=['Rank','CollectionName','Link'])
 
-content = wd.find_elements('xpath', "//tbody[@role='rowgroup']/tr[@class='!tw-border-b-[1px] !tw-border-gray-400 me-table-border']/td/a")
+for i in range(1,21):
+    Y = 50*i
+    scrollable_element = wd.find_element('xpath','//div[@class="dark-scroll-bar"]')
+    wd.execute_script(f"arguments[0].scrollBy(0, {Y});", scrollable_element)
 
-hrefs = [x.get_attribute('href') for x in content]
-texts = [x.text for x in content]
-ranking_places = [x.split('\n',1)[0] for x in texts]
-collection_names = [x.split('\n',1)[1] for x in texts]
-dict_for_df = {
-    'Rank': ranking_places,
-    'CollectionName': collection_names,
-    'Link': hrefs
-    }
-df = pd.DataFrame(dict_for_df) 
+    time.sleep(0.1)
+    content = wd.find_elements('xpath', "//tbody[@role='rowgroup']/tr[@class='!tw-border-b-[1px] !tw-border-gray-400 me-table-border']/td/a")
 
-df.to_csv('popular_collections.csv')
+    hrefs = [x.get_attribute('href') for x in content]
+    texts = [x.text for x in content]
+    ranking_places = [x.split('\n',1)[0] for x in texts]
+    collection_names = [x.split('\n',1)[1] for x in texts]
+    dict_for_df = {
+        'Rank': ranking_places,
+        'CollectionName': collection_names,
+        'Link': hrefs
+        }
+    df = pd.DataFrame(dict_for_df)
+    popular_collections = popular_collections.append(df, ignore_index=True)
+
+popular_collections.drop_duplicates(inplace=True)
+print(popular_collections)
+
+popular_collections.to_csv('popular_collections.csv')
